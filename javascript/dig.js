78cc15a44272ead6259e403ba8cde387e44dcc50
treinartz <thomas.reinartz@gmail.com>
2020-11-12 08:20:06 -0600

    Update dig.js

--- javascript/dig.js
+++ javascript/dig.js
@@ -1,31 +1,41 @@
-function setup() {
-  var canvas = createCanvas(480, 480);
-	fill(255, 0, 0);
-	background(0,255,0)
+var c = [];
 
+function setup() {
+  createCanvas(800, 600);
+
+  for (var i = 0; i < 10; i++) {
+    c[i] = new Cell(width / 2, height / 2);
+  }
 }
 
 function draw() {
-//head
-stroke(0);
-fill(235,221,150);
-ellipse(240, 240, 300, 300);
-//eyes
-	fill(0);
-ellipse(280, 220, 50, 50);
-ellipse(200, 220, 50, 50);
-	rect(190,196,100,10);
-//mouth
-	fill(255,0,0);
-rect(190,280,100,10);
-//Arrow
-	fill(0,255,255);
-	noStroke();
-rect(220,90,30,60);
-triangle(210,150,260,150,235,170);
-//text
-	textSize(32);
-	fill(0);
-text("Zach Simons",50,50);
-text("ACSCompSciPandemic2020 ",50,75);
+  background(0);
+  for (var i = 0; i < c.length; i++) {
+    c[i].move();
+    c[i].show();
+  }
+}
+class Cell {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  move() {
+    this.x += (int)(Math.random() * 5) - 2;
+    this.y += (int)(Math.random() * 5) - 2;
+    if (mouseX > this.x) {
+      this.x += (int)(Math.random() * 7);
+    } else if (mouseX < this.x) {
+      this.x -= (int)(Math.random() * 7);
+    }
+  }
+
+  show() {
+    fill(255);
+    ellipse(this.x, this.y, 30, 30);
+    fill(255, 0, 0);
+    textSize(22);
+    text("Hello", this.x, this.y);
+  }
 }
